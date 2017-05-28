82adcd02d7975530537e8c009184153af2e771a2
jrbeverly <jrbeverly@live.com>
2017-05-28 13:48:07 -0400

    Applying the new build template for docker based on proposal

---
+++ build/build-all.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -ex
+
+# Variables
+#
+# Variables of the script.
+SCRIPT=$(readlink -f "$0")
+DIR="$(dirname $SCRIPT)"
+ROOT_DIR="$(dirname $DIR)"
+BUILD_DIR="${ROOT_DIR}/build"
+
+# Rasterizing
+#
+# Rasterizes the scalable vector graphics.
+cd "${ROOT_DIR}/versions"
+for dirname in *
+do
+  test -d "$dirname" || continue
+
+  make -C "${BUILD_DIR}" VERSION=${dirname} build
+done
